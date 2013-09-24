e800d810d22470407e9f1d37118ac8258b6b01a1
Galliez Bruno <galliez.bruno@gmail.com>
2013-09-24 13:33:16 +0200

    Suppression d'un test erronné

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
 
-	$('body').on('onselectstart','#fLeft,#fRight',function(){
-			$.fn.disableSelection = function() {
-		        return this
-		                 .attr('unselectable', 'on')
-		                 .css('user-select', 'none')
-		                 .on('selectstart', false);
-		    };
-	});
-
 });
